43b5c8c1fbe6b217dbc2be94c2a0c05f3ed29a82
Scheider <s.scheider@uu.nl>
2018-11-24 11:20:25 +0100

    error corrected

--- rasteranalysis.py
+++ rasteranalysis.py
@@ -112,34 +112,34 @@ def generateCoverageRaster(name="parks"):
 
 """A function for looking up Raster cell row/colum projected in RD_new, based on a WGS84 coordinate pair as input as well as a Geo raster tile"""
 project = lambda x, y: pyproj.transform(pyproj.Proj(init='EPSG:4326'), pyproj.Proj(init='EPSG:28992'), x, y)
-def lookupWGS84(x,y,GeoT):
+def lookupWGS84(x,y,raster):
     p = shapely.geometry.point.Point(x,y)
     print(str(p))
-    pn = transform(project, p)
-    print(str(pn))
+    x,y = project(x,y)
+    print(x,y)
     try:
         # Find location of point (x,y) on raster, e.g. to extract info at that location
-        col, row = gr.map_pixel(pn.x,pn.y,GeoT[1],GeoT[-1], GeoT[0],GeoT[3])
-        #value = gr.map_pixel(pn.x,pn.y, )
+        #col, row = gr.map_pixel(pn.x,pn.y,GeoT[1],GeoT[-1], GeoT[0],GeoT[3])
+        value = raster.map_pixel(x,y)
     except:
         print "Coordinates out of bounds!"
-        return 'NN','NN'
-    return row, col
+        return 'NN'#,'NN'
+    return value
 
 """Enriches a track csv file with distances to landuse categories, both given as input. Writes out a novel csv file. If coordinates are out of bounds, returns NN"""
 def enrichtrack(csvf, categories, rastertype='dist2'):
     clist = categories.keys()
     #testpoint = 123907.175,  496407.706
     #print("\n  Mean: {}, Std: {}, max: {}, min: {}".format(str(data.mean()), str(data.std()),str(data.max()),str(data.min())))
-    NDV, xsize, ysize, GeoT, Projection, DataType = gr.get_geo_info("distrast\\dist2sports.tif")
-    data = gr.load_tiff("distrast\\dist2sports.tif")
-    s = data.shape
-    rows = s[0]
-    cols = s[1]
+    #NDV, xsize, ysize, GeoT, Projection, DataType = gr.get_geo_info("distrast\\dist2rsda.tif")
+    #data = gr.load_tiff("distrast\\dist2rsda.tif")
+    #s = data.shape
+    #rows = s[0]
+    #cols = s[1]
     #load raster data once
     rasters = []
     for c in clist:
-        rasters.append(gr.load_tiff("distrast\\"+rastertype+c+".tif"))
+        rasters.append(gr.from_file("distrast\\"+rastertype+c+".tif"))
     with open(csvf, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         with open(csvf.split('.')[0]+'en.csv', 'wb') as out:
@@ -149,14 +149,15 @@ def enrichtrack(csvf, categories, rastertype='dist2'):
                 x = float(line[1])
                 y = float(line[0])
                 valuelist =[]
-                row, col = lookupWGS84(x, y, GeoT)
-                if row != 'NN' and (row < rows and col < cols):
-                    for data in rasters:
-                        valuelist.append(str(data[row,col]))
+                #row, col = lookupWGS84(x, y, GeoT)
+                for data in rasters:
+                    value = lookupWGS84(x, y, data)
+                    if value != 'NN': #and (row < rows and col < cols):
+                        valuelist.append(str(value))
+                    else:
+                        print("Coordinates out of bounds!")
+                        valuelist.append(str('NN'))
                     writer.writerow([line[0], line[1]]+valuelist)
-                else:
-                    print("Coordinates out of bounds!")
-                    writer.writerow([line[0], line[1]]+['NN' for i in clist])
                 #if i ==20:
                 #    break
     csvfile.close()
@@ -318,13 +319,13 @@ def main():
     bbgcategories = {'rsda': [20],'parks':[40,42], 'agric':[51], 'sports':[41], 'recr': [43,44], 'frst':[60], 'ntr':[61,62], 'wtr':[70,71,72,73,74,75,76,77,78,80,81,82,83], 'trfc':[11]}
     """meaning of BBG 2012 landuse categories: rsda (residential areas), agric (agriculture areas), sports (sports areas) recr (recreation areas),frst (forest areas), ntr (nature areas), wtr (water areas), trfc (traffic areas)"""
     attributes = {'BEV_DICHTH':None,'P_65_EO_JR':None,'P_EENP_HH':None, 'P_HH_Z_K':None,'P_N_W_AL':None}
-    #bbgcategories = {'rsda': [20]} #residential areas
+    bbgcategories = {'rsda': [20]} #residential areas
     start = time.time()
     #for name,v in bbgcategories.items():
     #    generatedistraster(v,name)
     #    generateCoverageRaster(name)
     #store distances to these landuse areas for points in some track
-    enrichtrack('GPS.csv',bbgcategories,'covOf')
+    enrichtrack('GPS.csv',bbgcategories,'dist2')
     #enrichCBS('GPS.csv')
     #generateCBSStats()
     #generateCellStats(attributes, rastertype='r', sourcefolder = 'cellstats')
